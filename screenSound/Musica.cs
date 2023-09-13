cbfabbd3a52f6de11e4366ba4e0bdcc21d96dd96
Lucas Mateus <lucasmateus290903@gmail.com>
2023-09-12 21:08:07 -0300

    Adicionar arquivos de projeto.

---
+++ screenSound/Musica.cs
@@ -0,0 +1,24 @@
+﻿class Musica
+{
+    public string Nome { get; set; }
+    public string Artista { get; set; }
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}";
+
+    public void ExibirFixaTecnica()
+    {
+        Console.WriteLine($"\nNome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao} segundos");
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no Plano");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus++");
+        }
+
+    }
+}
---
+++ screenSound/Program.cs
@@ -0,0 +1,15 @@
+﻿Musica musica1 = new Musica();
+musica1.Nome = "Pais e Filhos";
+musica1.Artista = "Legião Urbana";
+musica1.Duracao = 304;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.DescricaoResumida);
+
+Musica musica2 = new Musica();
+musica2.Nome = "Dai a Cesar o que é de Cesar";
+musica2.Artista = "Cesar MC";
+musica2.Duracao = 306;
+musica2.Disponivel = false;
+
+musica1.ExibirFixaTecnica();
+musica2.ExibirFixaTecnica();
